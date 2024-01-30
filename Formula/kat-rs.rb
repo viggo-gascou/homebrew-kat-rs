642254c7b32b431953c1ccfc2b2bcfe885614203
Viggo Gascou <94069687+viggo-gascou@users.noreply.github.com>
2024-01-30 21:22:51 +0100

    Update kat-rs.rb

--- Formula/kat-rs.rb
+++ Formula/kat-rs.rb
@@ -13,16 +13,16 @@ class KatRs < Formula
   if OS.mac?
     if Hardware::CPU.intel?
       url "https://github.com/viggo-gascou/kat-rs/releases/download/v#{version}/kat-v#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "your_sha256_for_intel"
-    else
+      sha256 "59815a73efd51409b44b2d18e5d40e88b0fdac244cc46fbc6181c61e71099691"
+    elsif Hardware::CPU.arm?
       url "https://github.com/viggo-gascou/kat-rs/releases/download/v#{version}/kat-v#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "your_sha256_for_arm"
+      sha256 "796097a11a2b22355b16cbd1ce0f17bbadbc2140fcb01a3fd2fbdfd6f286a138"
     end
   end
 
   if OS.linux?
     url "https://github.com/viggo-gascou/kat-rs/releases/download/v#{version}/kat-v#{version}-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "your_sha256_for_linux"
+    sha256 "532f335578516c882640d5eb649a279338f9349d213410a682d72af46f6caa7e"
   end
 
   def install
