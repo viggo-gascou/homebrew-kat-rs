ce39f1ecb764cca8370aa30dc0b541b86ae2aa9c
Viggo Gascou <94069687+viggo-gascou@users.noreply.github.com>
2024-01-30 21:32:01 +0100

    Update kat-rs.rb

--- Formula/kat-rs.rb
+++ Formula/kat-rs.rb
@@ -9,18 +9,18 @@ class KatRs < Formula
     regex(/^v?(\d+(?:\.\d+)+)$/i)
   end
 
-  if OS.mac?
+ if OS.mac?
     if Hardware::CPU.intel?
-      url "https://github.com/viggo-gascou/kat-rs/releases/download/v#{version}/kat-v#{version}-x86_64-apple-darwin.tar.gz"
+      url "https://github.com/viggo-gascou/kat-rs/releases/download/v0.1.1/kat-v0.1.1-x86_64-apple-darwin.tar.gz"
       sha256 "59815a73efd51409b44b2d18e5d40e88b0fdac244cc46fbc6181c61e71099691"
     elsif Hardware::CPU.arm?
-      url "https://github.com/viggo-gascou/kat-rs/releases/download/v#{version}/kat-v#{version}-aarch64-apple-darwin.tar.gz"
+      url "https://github.com/viggo-gascou/kat-rs/releases/download/v0.1.1/kat-v0.1.1-aarch64-apple-darwin.tar.gz"
       sha256 "796097a11a2b22355b16cbd1ce0f17bbadbc2140fcb01a3fd2fbdfd6f286a138"
     end
   end
 
   if OS.linux?
-    url "https://github.com/viggo-gascou/kat-rs/releases/download/v#{version}/kat-v#{version}-x86_64-unknown-linux-gnu.tar.gz"
+    url "https://github.com/viggo-gascou/kat-rs/releases/download/v0.1.1/kat-v0.1.1-x86_64-unknown-linux-gnu.tar.gz"
     sha256 "532f335578516c882640d5eb649a279338f9349d213410a682d72af46f6caa7e"
   end
 
