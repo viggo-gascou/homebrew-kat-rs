7514447dddccda8421481af00ca4ff0a4d7966b5
Viggo Gascou <94069687+viggo-gascou@users.noreply.github.com>
2024-04-13 19:28:28 +0200

    Bump version to v.0.1.3 in url

--- Formula/kat-rs.rb
+++ Formula/kat-rs.rb
@@ -11,16 +11,16 @@ class KatRs < Formula
 
   if OS.mac?
     if Hardware::CPU.intel?
-      url "https://github.com/viggo-gascou/kat-rs/releases/download/v0.1.2/kat-v0.1.2-x86_64-apple-darwin.tar.gz"
+      url "https://github.com/viggo-gascou/kat-rs/releases/download/v0.1.3/kat-v0.1.3-x86_64-apple-darwin.tar.gz"
       sha256 "5429bf7fe22d79ff795d84c4fec957c21f6f1102084f70b8efe732124be2fcfc"
     elsif Hardware::CPU.arm?
-      url "https://github.com/viggo-gascou/kat-rs/releases/download/v0.1.2/kat-v0.1.2-aarch64-apple-darwin.tar.gz"
+      url "https://github.com/viggo-gascou/kat-rs/releases/download/v0.1.3/kat-v0.1.3-aarch64-apple-darwin.tar.gz"
       sha256 "cd3bbdda4dc7f0b3385f3b4589c8407a06a44cb65e26a7d8d1e6f3a00348ad7e"
     end
   end
 
   if OS.linux?
-    url "https://github.com/viggo-gascou/kat-rs/releases/download/v0.1.2/kat-v0.1.2-x86_64-unknown-linux-gnu.tar.gz"
+    url "https://github.com/viggo-gascou/kat-rs/releases/download/v0.1.3/kat-v0.1.3-x86_64-unknown-linux-gnu.tar.gz"
     sha256 "ecd1ea2ffec46bdbe91f3f3f8a1156ec1f352fc22bc155bd02fa43d17c0e2393"
   end
 
